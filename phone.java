af0a4d4069ca4d9c646426d68e7359c120f9fac3
mahmod Alabd <mahmud0798523364@gmail.com>
2026-04-05 21:40:22 +0300

    Add files via upload
    the oop code( the phone brand and storage)

---
+++ phone.java
@@ -0,0 +1,18 @@
+
+public class phone {
+String brand;
+int storage;
+
+phone(String brand,int storage){
+    this.brand=brand;
+    this.storage=storage;
+}
+void display(){
+    System.out.println("the brand :  "+brand+"     "+"the storage :  "+storage);
+}
+
+
+
+
+
+}
---
+++ phonecode.java
@@ -0,0 +1,12 @@
+
+public class phonecode {
+    public static void main(String[] args) {
+        phone p1=new phone("iphone",256);
+        System.out.println("----- phone specifications");
+        p1.display();
+        phone p2=new phone("samsung",128);
+        System.out.println("----- phone specifications");
+        p2.display();
+    }
+    
+}
