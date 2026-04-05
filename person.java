28e4694ab10775ab0d42e9ff9bd6db3f56eebd7e
mahmod Alabd <mahmud0798523364@gmail.com>
2026-04-05 20:23:19 +0300

    Add files via upload
    this is two code 
    the first code with out main (just print)
    .and the second code contanis the same code and includes the main method

---
+++ person.java
@@ -0,0 +1,24 @@
+
+public class person {
+String name;
+int age;
+
+
+person(String name,int age){
+this.name=name;
+this.age=age;
+
+}
+
+void display(){
+    System.out.println("name ; "+name+"age : "+age);
+}
+
+
+
+
+
+
+
+
+}
---
+++ persontest.java
@@ -0,0 +1,12 @@
+
+public class persontest {
+    public static void main(String[] args) {
+        person p1=new person("ata",20);
+        System.out.println("-------person 1 detaeles ");
+        p1.display();
+        person p2=new person("kamal",25);
+        System.out.println("-------person 2 detailes");
+        p2.display();
+    }
+    
+}
